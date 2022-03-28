3d2b57e03aab16b97b803da5ac15b7456c846866
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2022-03-28 16:38:55 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// CROSS PRODUCT RULE === numberTwo * numberThree = numberOne * RESULT
+  
+const crossCalculate = () => {
+  let numberOne = parseFloat(document.getElementById("numberOne").value);
+  let numberTwo = parseFloat(document.getElementById("numberTwo").value);
+  let numberThree = parseFloat(document.getElementById("numberThree").value);
+
+  // Resultat
+  document.getElementById("myResult").value = Math.round((numberTwo * numberThree) / numberOne * 100) / 100;
+}
+
+// Ajout de ENTER pour lancer crossCalculate()
+document.addEventListener('keydown', (event) => {
+  if(event.key === "Enter") {
+    crossCalculate();
+  }
+})
+
+// RESET
+
+const resetForm = () => {
+  const form = document.getElementById('.form')
+
+  document.addEventListener('click', () => {
+    console.log("prout")
+    form.reset();
+  })
+}
