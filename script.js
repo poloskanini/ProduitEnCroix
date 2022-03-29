1b7ad52654b47f5b13f580ae26c934fe73f3c32f
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2022-03-29 12:44:37 +0200

    (bf) Problem on myResult color on mobiles-4 + new condition for numbers only

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ const crossCalculate = () => {
   let numberThree = parseFloat(document.getElementById("numberThree").value);
 
   // Resultat
-  document.getElementById("myResult").value = Math.round((numberTwo * numberThree) / numberOne * 100) / 100;
+  if (Number.isInteger(numberOne && numberTwo && numberThree)) {
+    document.getElementById("myResult").value = Math.round((numberTwo * numberThree) / numberOne * 100) / 100;
+  } else {
+    alert('Veuillez entrer un nombre uniquement');
+  }
 }
 
 // Ajout de ENTER pour lancer crossCalculate()
 document.addEventListener('keydown', (event) => {
   if(event.key === "Enter") {
     crossCalculate();
+    event.preventDefault();
   }
 })
